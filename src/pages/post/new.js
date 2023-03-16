62c971948c17716288c36157f9204b83b8ed101a
Taras.Kravets <taraskravets95@gmail.com>
2023-03-16 22:29:43 +0200

    feat(sideBar): implemented sidebar

--- src/pages/post/new.js
+++ src/pages/post/new.js
@@ -1,4 +1,5 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { Layout } from '../../components/layout';
 
 export default function NewPost(props) {
   return (
@@ -7,6 +8,10 @@ export default function NewPost(props) {
     </>
   );
 }
+
+NewPost.getLayout = function getLayout(page, pageProps) {
+  return <Layout {...pageProps}>{page}</Layout>;
+};
 
 export const getServerSideProps = withPageAuthRequired(async () => {
   return {
