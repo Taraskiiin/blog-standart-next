f051c345f8d341f2e209d8cf42789f312671292a
Taras.Kravets <taraskravets95@gmail.com>
2023-03-16 18:59:39 +0200

    feat(UserData): getUser data from auth0

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,25 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function Home() {
+  const { user } = useUser();
+
+  console.log('user', user);
+
   return (
     <>
       <h1>this home</h1>
       <div>
-        <Link href='/api/auth/login'>Login</Link>
+        {!!user ? (
+          <>
+            <Image src={user.picture} alt={user.name} height={50} width={50} />
+            <div>{user.email}</div>
+            <Link href='/api/auth/logout'>Logout</Link>
+          </>
+        ) : (
+          <Link href='/api/auth/login'>Login</Link>
+        )}
       </div>
     </>
   );
