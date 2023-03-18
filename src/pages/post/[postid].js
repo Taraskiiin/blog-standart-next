6a53bdb181ebcd84b9659d53dbe1bab789df13cf
Taras.Kravets <taraskravets95@gmail.com>
2023-03-18 15:38:01 +0200

    feat(postPage): added postPage

--- src/pages/post/[postid].js
+++ src/pages/post/[postid].js
@@ -1,7 +1,12 @@
-import Head from 'next/head';
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { getSession } from "@auth0/nextjs-auth0";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { ObjectId } from "mongodb";
 
-export default function Post() {
+import clientPromise from "../../lib/mongoDB";
+import { Layout } from "../../components/layout";
+
+export default function Post(props) {
+  console.log(props);
   return (
     <>
       <h1>this Post</h1>
@@ -9,8 +14,41 @@ export default function Post() {
   );
 }
 
-export const getServerSideProps = withPageAuthRequired(async () => {
-  return {
-    props: {},
-  };
+Post.getLayout = function getLayout(page, pageProps) {
+  return <Layout {...pageProps}>{page}</Layout>;
+};
+
+export const getServerSideProps = withPageAuthRequired({
+  async getServerSideProps(ctx) {
+    const userSession = await getSession(ctx.req, ctx.res);
+    const client = await clientPromise;
+    const db = client.db("roblog-next");
+
+    const user = await db.collection("users").findOne({
+      auth0id: userSession.user.sub,
+    });
+
+    const post = await db.collection("posts").findOne({
+      _id: new ObjectId(ctx.params.postId),
+      userId: user._id,
+    });
+
+    if (!post) {
+      return {
+        redirect: {
+          destination: "/post/new",
+          permanent: false,
+        },
+      };
+    }
+
+    return {
+      props: {
+        postContent: post.postContent,
+        title: post.title,
+        metaDescription: post.metaDescription,
+        keywords: post.keywords,
+      },
+    };
+  },
 });
